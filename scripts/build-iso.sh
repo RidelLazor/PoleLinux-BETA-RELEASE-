3311c3fd2b10bdcbcabbe57340ec6639fdce738e
ridellazor <danishfauza9@gmail.com>
2026-05-31 04:29:51 +0000

    fix: use modern bootmode names bios.syslinux + uefi.grub for mkarchiso

--- scripts/build-iso.sh
+++ scripts/build-iso.sh
@@ -51,9 +51,7 @@ iso_application="PoleLinux Live"
 iso_version="1.0"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
-           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
+bootmodes=('bios.syslinux' 'uefi.grub')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
