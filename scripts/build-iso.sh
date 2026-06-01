f4dcd21cd7b04443659f7d5c1124c4200282dfc5
ridellazor <danishfauza9@gmail.com>
2026-06-01 04:20:37 +0000

    fix: add boot=live components to kernel cmdline (was overriding default)

--- scripts/build-iso.sh
+++ scripts/build-iso.sh
@@ -26,7 +26,7 @@ LB_CONFIG_OPTS=(
   --distribution bookworm
   --architectures amd64
   --archive-areas "main contrib non-free non-free-firmware"
-  --bootappend-live "quiet splash"
+  --bootappend-live "boot=live components quiet splash"
   --bootappend-install ""
   --debian-installer none
   --iso-volume "PoleLinux"
